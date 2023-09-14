37d9b3f093444e90d1fdf693f347f2b97ff182e7
subash <bhandarisubash22@gmail.com>
2023-09-14 21:56:36 +0545

    square added

---
+++ src/Board.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Square from './Square'
+import './App.css'
+
+function Board() {
+  return (
+   <>
+   <div className="board">
+   <Square/>
+   </div>
+   </>
+  )
+}
+
+export default Board
